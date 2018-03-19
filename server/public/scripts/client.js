c04d5dc8cc402a6b1d856b11a73f323e64b790ca
Atticus Pomerantz <rapomerantz@gmail.com>
2018-03-19 09:57:14 -0500

    second commit

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,8 +1,29 @@
-console.log('js'); 
-
 $(document).ready(readyNow); 
 
 function readyNow () {
-    console.log('jq');
+    $('#clearButton').on('click', function() {
+        console.log('in clearButton on click');
+    });// end clearButton on click
     
-}
+    
+    $('#doMathButton').on('click', function() {
+        console.log(' in doMathButton on click');
+        //capture user input
+        let x = $('#xIn').val();
+        let y = $('#yIn').val();
+        let type = $('#typeIn').val();
+        //store in an object
+        let objectToSend = {
+            x: x,
+            y: y,
+            type: type,
+        };
+        console.log('object to send:', objectToSend);
+        //send object to server
+        //display answer on DOM 
+        //
+
+
+
+    }); //end #doMathButton on click
+}//END READY NOW 
--- server/server.js
+++ server/server.js
@@ -16,4 +16,4 @@ let port = 4567;
 //spin up server
 app.listen( port, function () {
     console.log('server up on:', port); 
-}); //end spin up server`
+}); //end spin up server
